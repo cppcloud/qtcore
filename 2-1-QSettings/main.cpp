325d09c6ca21c8ba5fcb87f2823b616de21f0c6e
cppcloud <admin@cplusplus.cloud>
2024-07-01 03:31:40 +0800

    QSettings

---
+++ 2-1-QSettings/main.cpp
@@ -0,0 +1,24 @@
+#include <QCoreApplication>
+#include <QDebug>
+#include <QSettings>
+
+int main(int argc, char *argv[])
+{
+    QCoreApplication a(argc, argv);
+
+    QCoreApplication::setOrganizationName("VoidRealms");
+    QCoreApplication::setOrganizationDomain("VoidRealms.com");
+    QCoreApplication::setApplicationName("Tester");
+
+    QSettings settings(QCoreApplication::organizationName(), QCoreApplication::applicationName());
+
+    //Save the setting
+    //settings.setValue("test", 123);
+
+    //Read the setting back
+    qInfo() << settings.value("test").toString();
+    qInfo() << settings.value("test").toInt();
+
+
+    return a.exec();
+}
