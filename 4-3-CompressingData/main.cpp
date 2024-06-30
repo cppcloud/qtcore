f580557b1acef90d58691efa4a359d3ad5667f22
cppcloud <admin@cplusplus.cloud>
2024-07-01 04:04:50 +0800

    CompressingData

---
+++ 4-3-CompressingData/main.cpp
@@ -0,0 +1,27 @@
+#include <QCoreApplication>
+#include <QDebug>
+
+QByteArray makeData() {
+    QByteArray data;
+
+    for(int i = 0; i < 1000; i++) {
+        data.append(QString::number(i) + "\r\n");
+    }
+
+    return data;
+}
+
+int main(int argc, char *argv[])
+{
+    QCoreApplication a(argc, argv);
+
+    QByteArray data = makeData();
+    QByteArray compressed = qCompress(data);
+    QByteArray decompressed = qUncompress(compressed);
+
+    qInfo() << "Original: " << data.length();
+    qInfo() << "Compressed: " << compressed.length();
+    qInfo() << "Decompressed: " << decompressed.length();
+
+    return a.exec();
+}
