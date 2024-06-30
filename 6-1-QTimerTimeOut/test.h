66cd07adfaf95d7b29d935d6fa84833dfcf26016
cppcloud <admin@cplusplus.cloud>
2024-07-01 04:25:21 +0800

    QTimerTimeOut

---
+++ 6-1-QTimerTimeOut/test.h
@@ -0,0 +1,26 @@
+#ifndef TEST_H
+#define TEST_H
+
+#include <QObject>
+#include <QDebug>
+#include <QTimer>
+#include <QTime>
+
+class test : public QObject
+{
+    Q_OBJECT
+public:
+    explicit test(QObject *parent = nullptr);
+
+signals:
+
+public slots:
+    void timeout();
+    void dostuff();
+
+private:
+    QTimer timer;
+    int number;
+};
+
+#endif // TEST_H
